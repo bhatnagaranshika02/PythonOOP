094053f541c87c2f3a0f9cea3011783ee462520f
bhatnagaranshika02 <bhatnagaranshika02@gmail.com>
2020-11-21 21:52:52 +0530

    new

--- singleinh.py
+++ singleinh.py
@@ -3,23 +3,23 @@ class vehicle:
         self.color = color
 
     def display(self):
-        print('vehicle: ',self.color)
+        print('vehicle: ',self.color,self.number)
 
 class TwoWheeler(vehicle):
     def __init__ (self,number,color):
         #vehicle.__init__(self,color)
-        super()
+        super().__init__(color)
 
         self.number = number
         self.color = color
 
         
 
-    def printing(self):
+    def display(self):
         print(self.color ,'and', self.number)
 
 
 obj1 = TwoWheeler(24,'pink')
 obj1.display()
+vehicle.display(obj1)
 
-obj1.printing()
