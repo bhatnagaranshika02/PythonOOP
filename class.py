11162e12f93be5f45c1a2daaf44bd3313920498e
bhatnagaranshika02 <bhatnagaranshika02@gmail.com>
2020-11-21 21:12:52 +0530

    new

--- class.py
+++ class.py
@@ -15,9 +15,17 @@ class Demo:
 
 
 d= Demo(10,20)
+d1 = Demo(30,40)
 print(Demo.i)
 print(d.i)
 print(d.personal())
 print(Demo.personal())
 print(d.display())
+d=d1
+print(d.display())
+print(d1.display())
+print(Demo.__dict__)
+print(Demo.__name__)
+print(Demo.__base__)
+print(Demo.__doc__)
         
